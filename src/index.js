fe65ecc43716004a5029221d2d20e866a28feae0
Julio <julio.chazari@gmail.com>
2020-12-18 23:48:52 -0600

    Imported global styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { GlobalStyle } from './reset.css';
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <GlobalStyle />
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
