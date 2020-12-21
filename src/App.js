a258ee1cbdca75f1f25a8da8c584cc623e49e5df
Julio <julio.chazari@gmail.com>
2020-12-20 22:01:30 -0600

    Layed out all the components for the basic structure of the site

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React from 'react'
-import { GlobalStyle } from './assets/styles';
+import { Container, Row, Col } from './components/Layout';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import InputGroup from './components/InputGroup';
+import Searcher from './components/Searcher';
+import MovieCard from './components/MovieCard';
 
 function App() {
   return (
-    <>
-      <GlobalStyle />
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
-    </>
+    <div className="App">
+      <Navbar />
+      <Header>
+        <Container>
+          <Header.Title>Welcome!</Header.Title>
+          <Header.SubTitle>Search thousands of movies and nominate the ones you love!</Header.SubTitle>
+        </Container>
+      </Header>
+      <Container>
+        <InputGroup mt="24px" mb="48px">
+          <InputGroup.Label htmlFor="movie-searcher">Movie Search</InputGroup.Label>
+          <Searcher type="text" placeholder="Search for a movie by title" id="movie-searcher" />
+        </InputGroup>
+        <Row>
+          <Col>
+            <MovieCard />
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
