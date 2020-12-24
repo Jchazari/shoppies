a2c7ded08e76f08914dd5eafcf084550c0b51dc5
Julio <julio.chazari@gmail.com>
2020-12-24 11:17:21 -0600

    Main landing header, 98% completed, may change background / text

---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  min-height: 300px;
+  padding: 0 16px;
+  text-align: center;
+
+  h1 {
+    font-size: 2.75rem;
+    line-height: 1.2;
+    color: #fed130;
+  }
+
+  h2 {
+    font-size: 1.75rem;
+    font-weight: 500;
+    line-height: 1;
+  }
+
+  @media (max-width: 640px) {
+    min-height: 275px;
+    
+    h1 {
+      font-size: 2.5rem;
+    }
+
+    h2 {
+      font-size: 1.5rem;
+    }
+  }
+`;
+
+export default Header;
