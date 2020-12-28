086cae29fb4a81f2e1dede44464afc739d84ed4e
Julio <julio.chazari@gmail.com>
2020-12-28 15:27:17 -0600

    Added 'remove' nominated movie functionality and refactored to use updated components such ass CSS Grid

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import { fakeMoviesData } from './fakeMovies';
+import styled from 'styled-components';
 import { BASE_URL } from './constants';
-import { ReactComponent as Trophy } from './assets/trophy.svg';
 import useDebounce from './hooks/useDebounce';
+import Container from './components/Container';
+import Grid from './components/Grid';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Nominees from './components/Nominees';
-import Container from './components/Container';
-import Row from './components/Row';
-import Col from './components/Col';
+import MovieCard from './components/MovieCard';
+import Skeleton from './components/Skeleton';
 import InputGroup from './components/InputGroup';
 import Input from './components/Input';
 import Button from './components/Button';
-import MovieCard from './components/MovieCard';
-import Skeleton from './components/Skeleton';
+
+const Section = styled.section`
+  width: 100%;
+  min-height: 100vh;
+  padding: 16px 0;
+`;
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -65,6 +69,15 @@ function App() {
     setNominatedMovies([...nominatedMovies, movie]);
   }
 
+  function unNominateMovie(movie) {
+    const nominatedMoviesCopy = nominatedMovies.filter(mov => mov !== movie);
+    setNominatedMovies(nominatedMoviesCopy);
+  }
+
+  function isNominated(movie) {
+    return nominatedMovies.some(mov => mov.imdbID === movie.imdbID);
+  }
+
   function renderSkeletons() {
     return [...Array(6)].map((s, i) => <Skeleton key={i} />);
   }
@@ -76,28 +89,54 @@ function App() {
         <h1>Welcome!</h1>
         <h2>Search thousands of movies and nominate the ones you love!</h2>
       </Header>
-      <Nominees />
-      <Container>
-        <InputGroup mt="24px" mb="48px">
-          <label htmlFor="movie-searcher">Movie Search</label>
-          <Input
-            type="text"
-            placeholder="Search for a movie by title"
-            id="movie-searcher"
-            autoComplete="off"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-        </InputGroup>
-        <Row>
-          {fakeMoviesData.map(movie => (
-            <Col>
-              <MovieCard movie={movie} />
-              <Button mt="4px" icon={<Trophy />} full>Nominate</Button>
-            </Col>
-          ))}
-        </Row>
-      </Container>
+      <Nominees>
+        {nominatedMovies.map(movie => (
+          <div key={movie.imdbID}>
+            <MovieCard movie={movie} mb="5px" />
+            <Button
+              full
+              onClick={() => unNominateMovie(movie)}
+            >
+              Remove
+            </Button>
+          </div>
+        ))}
+      </Nominees>
+      <Section>
+        <Container>
+          <InputGroup>
+            <label htmlFor="movie-searcher">Movie Search</label>
+            <Input
+              type="text"
+              placeholder="Search for a movie by title"
+              id="movie-searcher"
+              autoComplete="off"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </InputGroup>
+          <Grid>
+            {isLoading ? (
+              renderSkeletons()
+            ) : isError ? (
+              <p>No Results 😔</p>
+            ) : (
+              movies.map(movie => (
+                <div key={movie.imdbID}>
+                  <MovieCard movie={movie} mb="5px" />
+                  <Button
+                    full
+                    disabled={isNominated(movie)}
+                    onClick={() => nominateMovie(movie)}
+                  >
+                    Nominate
+                  </Button>
+                </div>
+              ))
+            )}
+          </Grid>
+        </Container>
+      </Section>
     </div>
   );
 }
