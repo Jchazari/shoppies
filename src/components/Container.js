105c41b4a2e1ba64eb568e05641d6c000f2d1679
Julio <julio.chazari@gmail.com>
2020-12-28 15:16:43 -0600

    Updated max-width, refactored code

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,19 +1,10 @@
-import React from 'react'
 import styled from 'styled-components';
 
-const StyledContainer = styled.div`
+const Container = styled.div`
   width: 100%;
-  max-width: 1176px;
+  max-width: 1200px;
   padding: 0 16px;
   margin: 0 auto;
 `;
-
-function Container({ children }) {
-  return (
-    <StyledContainer>
-      {children}
-    </StyledContainer>
-  );
-}
 
 export default Container;
