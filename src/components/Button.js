c0e67e522da51ebbf1843b6fd224dbb0af11f471
Julio <julio.chazari@gmail.com>
2020-12-28 15:21:55 -0600

    Removed icon styles, updated buttong bg

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,38 +1,27 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
-
-const buttonIconStyles = css`
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  svg {
-    margin-right: 8px;
-    width: 1.5em;
-    height: 1.5em;
-  }
-`;
+import styled from 'styled-components';
 
 const StyledButton = styled.button`
-  ${p => p.icon && buttonIconStyles}
-  width: ${p => p.full && '100%'};
   min-width: 64px;
+  width: ${props => props.full && '100%'};
   border: none;
   border-radius: 4px;
   padding: 12px 0;
-  margin-top: ${p => p.mt};
-  margin-bottom: ${p => p.mb};
-  background-color: #20232b;
+  margin-top: ${props => props.mt};
+  margin-bottom: ${props => props.mb};
+  background-color: #1f1f23;
   font-size: 0.875rem;
   font-weight: 600;
   color: #b9b9ff;
   box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
               0px 2px 2px 0px rgba(0, 0, 0, 0.14),
               0px 1px 5px 0px rgba(0,0,0,.12);
+  user-select: none;
   cursor: pointer;
   transition: all 0.2s;
 
   &:hover {
-    background-color: #272c38;
+    background-color: #20232b;
     box-shadow: 0px 3px 5px -2px rgba(0, 0, 0, 0.2),
                 0px 2px 5px 0px rgba(0, 0, 0, 0.14),
                 0px 1px 7px 0px rgba(0,0,0,.12);
