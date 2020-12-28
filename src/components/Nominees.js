12df9e3d912397339bb0aee557f88da12ce57fe1
Julio <julio.chazari@gmail.com>
2020-12-28 15:24:07 -0600

    Removed flex-box elements and replaced with CSS Grid

--- src/components/Nominees.js
+++ src/components/Nominees.js
@@ -1,83 +1,75 @@
 import React from 'react'
-import styled from 'styled-components';
-import Container from './Container';
-import Row from './Row';
-import Col from './Col';
-import MovieCard from './MovieCard';
-import Button from './Button';
-
-// const StyledNominees = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   width: 100%;
-//   overflow-x: auto;
-  
-//   @media (max-width: 915px) {
-//     justify-content: flex-start;
-//   }
-// `;
+import styled from 'styled-components/macro';
+import { ReactComponent as Trophy } from '../assets/trophy.svg';
+import Grid from './Grid';
 
 const Section = styled.section`
   position: relative;
   width: 100%;
-  padding: 16px 0;
-  background-color: #18191f;
-  overflow: hidden;
+  height: 395px;
+  padding: 16px calc(((100vw - 1200px) / 2) - 8px);
+  background-color: #111215;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.5);
 `;
 
 const MoviesContainer = styled.div`
   display: flex;
   justify-content: center;
+  position: relative;
+  width: 100%;
   margin-top: 16px;
-  overflow-x: auto;
-  overflow-y: visible;
-  -ms-overflow-style: none;
-  scrollbar-width: none;
-
-  &::-webkit-scrollbar {
-    display: none;
-  }
-
-  @media (max-width: 1010px) {
-    justify-content: flex-start;
-  }
+  overflow-x: hidden;
+  overscroll-behavior-x: contain;
 `;
 
-const Title = styled.h2`
-  font-size: 1.25rem;
-  font-weight: 500;
+const Placeholder = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  color: #373746;
+`;
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0 16px;
+
+  h2 {
+    position: relative;
+    font-size: 1.25rem;
+    font-weight: 500;
+  }
+
+  svg {
+    width: 1.75em;
+    height: 1.75em;
+    margin-right: 8px;
+  }
 `;
 
 function Nominees({ children }) {
   return (
     <Section>
-      <Container>
-        <Title>Your Nominations</Title>
+      <Header>
+        <Trophy />
+        <h2>Your Nominations</h2>
+      </Header>
+      {children.length === 0 ? (
+        <Placeholder>
+          <p>Begin by searching for a movie!</p>
+        </Placeholder>
+      ) : (
         <MoviesContainer>
-          <Row nowrap>
-            <Col fixed>
-              <MovieCard />
-              <Button mt="4px" full>Remove</Button>
-            </Col>
-            <Col fixed>
-              <MovieCard />
-              <Button mt="4px" full>Remove</Button>
-            </Col>
-            <Col fixed>
-              <MovieCard />
-              <Button mt="4px" full>Remove</Button>
-            </Col>
-            <Col fixed>
-              <MovieCard />
-              <Button mt="4px" full>Remove</Button>
-            </Col>
-            <Col fixed>
-              <MovieCard />
-              <Button mt="4px" full>Remove</Button>
-            </Col>
-          </Row>
+          <Grid fixed>
+            {children}
+          </Grid>
         </MoviesContainer>
-      </Container>
+      )}
     </Section>
   );
 }
