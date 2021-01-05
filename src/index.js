27aafebd20c1f84573fe6feae8b27c412dbf4a9a
Julio <julio.chazari@gmail.com>
2021-01-05 12:34:39 -0600

    Moved globalstyles from this file to App.js file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { GlobalStyle } from './reset.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
