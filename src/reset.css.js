0ded2fe3faf72cb7282e0c2a64503c90fe0cf182
Julio <julio.chazari@gmail.com>
2021-01-05 12:33:40 -0600

    Changed to export default

--- src/reset.css.js
+++ src/reset.css.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
-export const GlobalStyle = createGlobalStyle`
+const GlobalStyle = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
@@ -19,3 +19,5 @@ export const GlobalStyle = createGlobalStyle`
     overscroll-behavior: none;
   }
 `;
+
+export default GlobalStyle;
