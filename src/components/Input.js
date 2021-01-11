64e8f36c598ab6003b2a401274432d4c9ba63f9f
Julio <julio.chazari@gmail.com>
2021-01-11 17:14:57 -0600

    Code cleanup

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,19 +1,13 @@
 import React from 'react'
 import styled from 'styled-components/macro';
 
-const StyledInput = styled.input`
+const Input = styled.input`
   width: 100%;
-  padding: 12px 16px;
   border: none;
   border-radius: 5px;
+  padding: 12px 16px;
   box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0,0,0,.12);
   font-size: 16px;
 `;
-
-function Input({ ...props }) {
-  return (
-    <StyledInput {...props}/>
-  );
-}
 
 export default Input;
