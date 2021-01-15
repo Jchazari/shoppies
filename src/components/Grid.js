33dfda1dc36a6f2f5faac6c70f991a3f4478c795
Julio <julio.chazari@gmail.com>
2021-01-14 23:50:32 -0600

    className styles added so grid component can be used anywhere

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,50 +1,62 @@
-import styled, { css } from 'styled-components';
-
-const FixedGridStyles = css`
-  grid-template-columns: repeat(100, 180px);
-  overflow: auto;
-  scroll-behavior: smooth;
-  padding-bottom: 4px;
-
-  @media (max-width: 1200px) {
-    padding-left: 16px;
-    &:after {
-      content: '';
-      box-sizing: border-box;
-      width: 1px;
-    }
-  }
-
-  &::-webkit-scrollbar {
-    display: none;
-  }
-`;
-
-const ResponsiveGridStyles = css`
-  width: 100%;
-  grid-template-columns: repeat(6, 1fr);
-
-  @media (max-width: 1100px) {
-    grid-template-columns: repeat(5, 1fr);
-  }
-
-  @media (max-width: 950px) {
-    grid-template-columns: repeat(4, 1fr);
-  }
-
-  @media (max-width: 750px) {
-    grid-template-columns: repeat(3, 1fr);
-  }
-
-  @media (max-width: 580px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
-`;
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 const Grid = styled.div`
   display: grid;
-  grid-gap: 1rem;
-  ${props => props.fixed ? FixedGridStyles : ResponsiveGridStyles};
+
+  &.welcome-content {
+    grid-gap: 3rem;
+    grid-template-columns: 55% auto;
+    place-items: center;
+
+    @media (max-width: 830px) {
+      grid-template-columns: 1fr;
+    }
+  }
+
+  &.movie-search-results {
+    width: 100%;
+    grid-gap: 1rem;
+    grid-template-columns: repeat(6, 1fr);
+
+    @media (max-width: 1100px) {
+      grid-template-columns: repeat(5, 1fr);
+    }
+
+    @media (max-width: 950px) {
+      grid-template-columns: repeat(4, 1fr);
+    }
+
+    @media (max-width: 750px) {
+      grid-template-columns: repeat(3, 1fr);
+    }
+
+    @media (max-width: 580px) {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+
+  &.nominated-movies {
+    grid-gap: 1rem;
+    grid-template-columns: repeat(100, 180px);
+    overflow: auto;
+    scroll-behavior: smooth;
+    padding-bottom: 4px;
+
+    &::-webkit-scrollbar {
+      display: none;
+    }
+
+    @media (max-width: 1200px) {
+      padding-left: 16px;
+
+      &:after {
+        content: '';
+        box-sizing: border-box;
+        width: 1px;
+      }
+    }
+  }
 `;
 
 export default Grid;
