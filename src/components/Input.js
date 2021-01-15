1d4d26facc3dbad2b71e2e159c5da16334f6154b
Julio <julio.chazari@gmail.com>
2021-01-14 23:51:19 -0600

    Added border color and box-shadow on focus

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import styled from 'styled-components/macro';
+import styled from 'styled-components';
 
 const Input = styled.input`
   width: 100%;
-  border: none;
+  border: 1px solid #cad8ff;
   border-radius: 5px;
   padding: 12px 16px;
-  box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0,0,0,.12);
   font-size: 16px;
+  transition: box-shadow 0.2s;
+
+  &:focus {
+    box-shadow: 0 0 0 2px #cad8ff;
+  }
 `;
 
 export default Input;
