a153c7b74cd8071471de875009af3247aa2e9c6a
Julio <julio.chazari@gmail.com>
2021-01-15 02:10:29 -0600

    Removed variant prop and now using style inheritance

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,21 @@
 import PropTypes from 'prop-types';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
-const CTAButtonStyles = css`
-  background-color: #FFFF00;
-  color: #333;
-`;
-
-const DefaultButtonStyles = css`
+export const Button = styled.button`
+  min-width: 64px;
+  width: ${props => (props.full && '100%')};
+  margin-top: ${props => (props.mt)};
+  margin-bottom: ${props => (props.mb)};
+  padding: ${props => (props.size === 'lg' ? '14px 24px' : '12px 16px')};
+  font-size: ${props => (props.size === 'lg' ? '1rem' : '0.875rem')};
+  font-weight: 600;
+  border: none;
+  border-radius: 4px;
+  box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0,0,0,.12);
   background-color: #1d1f25;
   color: #cad8ff;
+  user-select: none;
+  cursor: pointer;
 
   &:disabled {
     background-color: #20232b;
@@ -17,40 +24,23 @@ const DefaultButtonStyles = css`
   }
 `;
 
-function variantStyles(variant) {
-  switch (variant) {
-    case 'cta':
-      return CTAButtonStyles;
-    default:
-      return DefaultButtonStyles;
-  }
-}
+export const CTAButton = styled(Button)`
+  background-color: #FFFF00;
+  color: #333;
 
-const Button = styled.button`
-  ${props => (variantStyles(props.variant))};
-  width: ${props => (props.full && '100%')};
-  margin-top: ${props => (props.mt)};
-  margin-bottom: ${props => (props.mb)};
-  padding: ${props => (props.size === 'lg' ? '14px 24px' : '12px 16px')};
-  font-size: ${props => (props.size === 'lg' ? '1rem' : '0.875rem')};
-  font-weight: 600;
-  min-width: 64px;
-  border: none;
-  border-radius: 4px;
-  box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0,0,0,.12);
-  user-select: none;
-  cursor: pointer;
+  &:disabled {
+    background-color: #FFFF00;
+    color: #dede00;
+  }
 `;
 
 Button.propTypes = {
   full: PropTypes.bool,
-  variant: PropTypes.oneOf(["default", "cta"]),
   size: PropTypes.string,
+  mt: PropTypes.string,
+  mb: PropTypes.string,
 };
 
 Button.defaultProps = {
   full: false,
-  variant: "default",
 };
-
-export default Button;
