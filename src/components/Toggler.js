9e2768b424c508e584f8f66bc0cbcc3251c3f107
Julio <julio.chazari@gmail.com>
2021-01-16 03:44:51 -0600

    Counter turns into green check when finished

--- src/components/Toggler.js
+++ src/components/Toggler.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
-import { ReactComponent as Icon } from '../assets/trophy.svg';
+import { ReactComponent as Check } from '../assets/check.svg';
+import { ReactComponent as Trophy } from '../assets/trophy.svg';
 
-const StyledToggler = styled.div`
+const TogglerButton = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -10,12 +11,18 @@ const StyledToggler = styled.div`
   bottom: 16px;
   width: 60px;
   height: 60px;
+  border: none;
   border-radius: 50%;
-  background-color: #232731;
+  background-color: #15161b;
   box-shadow: 0 2px 4px 1px rgba(0, 0, 0, 0.4);
   z-index: 999;
   user-select: none;
+  transition: background-color 0.2s;
   cursor: pointer;
+
+  &:hover {
+    background-color: #191a21;
+  }
 `;
 
 const Counter = styled.div`
@@ -28,20 +35,29 @@ const Counter = styled.div`
   width: 20px;
   height: 20px;
   border-radius: 50%;
-  background: red;
+  background: ${props => (props.count === 5 ? '#90ee90' : '#252833')};
+  color: #ffffff;
+  box-shadow: 0 2px 4px 1px rgba(0, 0, 0, 0.4);
 `;
 
-const Trophy = styled(Icon)`
+const TrophyIcon = styled(Trophy)`
   width: 40px;
   fill: #ffff00;
 `;
 
-function Toggler({ nominationCount, ...props }) {
+const CheckIcon = styled(Check)`
+  width: 10px;
+  fill: #ffffff;
+`;
+
+function Toggler({ count, ...props }) {
   return (
-    <StyledToggler {...props}>
-      <Counter>{nominationCount}</Counter>
-      <Trophy />
-    </StyledToggler>
+    <TogglerButton {...props}>
+      <Counter count={count}>
+        {count === 5 ? <CheckIcon /> : count}
+      </Counter>
+      <TrophyIcon />
+    </TogglerButton>
   );
 }
 
