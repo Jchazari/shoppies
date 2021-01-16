629cd8b2c2365e256f7534085edbf69f9b81d001
Julio <julio.chazari@gmail.com>
2021-01-16 03:46:08 -0600

    Banner is now just a simple message that is displayed in nominees section

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,69 +1,47 @@
-import React from 'react'
 import styled from 'styled-components';
+import { ReactComponent as CheckIcon } from '../assets/check.svg';
 
 const StyledBanner = styled.div`
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  max-width: 700px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   width: 100%;
-  height: 400px;
-  margin: auto auto;
   padding: 16px;
-  border: 1px solid #27272d;
+  margin-bottom: 16px;
   border-radius: 5px;
-  background-color: #1f1f23;
-  box-shadow: 0 0 12px 6px rgba(0, 0, 0, 0.2);
-  transform: ${props => props.isActive ? 'translateY(0)' : 'translateY(100vh)'};
-  transition: transform 0.2s;
-
-  @media (max-width: 768px) {
-    top: initial;
-    bottom: 0;
-    max-width: 100%;
-    height: 50vh;
-    margin: 0;
-    border-radius: 5px 5px 0 0;
-  }
+  background-color: #0e0e10;
 `;
 
-// const IconWrapper = styled.div`
-//   position: absolute;
-//   top: 10px;
-//   right: 10px;
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   width: 24px;
-//   height: 24px;
-//   padding: 5px;
-//   border-radius: 3px;
-//   cursor: pointer;
-//   transition: background-color 0.2s;
-//   &:hover { background-color: rgba(255, 255, 255, 0.1); }
+const IconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 70px;
+  height: 70px;
+  padding: 15px;
+  border-radius: 50%;
+  background-color: #90ee90;
+`;
 
-//   @media (max-width: 768px) {
-//     top: 5px;
-//     right: 5px;
-//     width: 32px;
-//     height: 32px;
-//     padding: 6px;
-//     border-radius: 0;
-//     &:hover {background-color: transparent;}
-//   }
-// `;
+const Check = styled(CheckIcon)`
+  width: 100%;
+  fill: #ffffff;
+`;
 
-// const CloseIcon = styled(Close)`
-//   width: 100%;
-//   height: 100%;
-//   path { fill: #d0d0d0; }
-// `;
+const Title = styled.h1`
+  font-size: 32px;
+  font-weight: 600;
+  color: #ffff00;
+`;
 
-function Banner({ isActive, setBannerActive }) {
+function Banner() {
   return (
-    <StyledBanner isActive={isActive}>
+    <StyledBanner>
+      <IconWrapper>
+        <Check />
+      </IconWrapper>
+      <Title>All Finished!</Title>
+      <p>Your 5 nominees are picked!</p>
     </StyledBanner>
   );
 }
